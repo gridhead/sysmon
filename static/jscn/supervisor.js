45995323546094b0ee5b04276b61b91bce570131
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2020-12-12 05:09:08 +0530

    Added live graphs for monitoring memory performance
    Made graphs transparent to facilitate theming and reworked tab switching
    
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- static/jscn/supervisor.js
+++ static/jscn/supervisor.js
@@ -4,7 +4,7 @@ let grafstyl = {
     maxValue: 100,
     grid: {
         strokeStyle: '#c0c0c0',
-        fillStyle: '#ffffff',
+        fillStyle: 'rgba(0, 0, 0, 0)',
         lineWidth: 1,
         millisPerLine: 250,
         verticalSections: 10,
@@ -20,11 +20,28 @@ let linestyl = {
     lineWidth: 2
 };
 
-function SwitchTab(head, body) {
-    $(".item").removeClass("active");
-    $(".bottom.attached").removeClass("active");
+function SwitchTab(head, sect) {
+    let sectlist = {
+        "cpuu": {
+            "cpuu-usej-ttle": "cpuu-usej-body",
+            "cpuu-cycl-ttle": "cpuu-cycl-body",
+            "cpuu-time-ttle": "cpuu-time-body",
+            "cpuu-stat-ttle": "cpuu-stat-body"
+        },
+        "memo": {
+            "memo-over-ttle": "memo-over-body",
+            "memo-phys-ttle": "memo-phys-body",
+            "memo-virt-ttle": "memo-virt-body",
+        }
+    };
+    for (indx in sectlist[sect]) {
+        if (indx !== head) {
+            $("#" + indx).removeClass("active");
+            $("#" + sectlist[sect][indx]).removeClass("active");
+        }
+    }
     $("#" + head).addClass("active");
-    $("#" + body).addClass("active");
+    $("#" + sectlist[sect][head]).addClass("active");
 }
 
 function AskForWebSocketEndpoint() {
@@ -45,14 +62,20 @@ async function OverviewGraphAJAX() {
     let physgraf = new SmoothieChart(grafstyl);
     let swapgraf = new SmoothieChart(grafstyl);
     let battgraf = new SmoothieChart(grafstyl);
+    let dediphys = new SmoothieChart(grafstyl);
+    let dedivirt = new SmoothieChart(grafstyl);
     let cpusline = new TimeSeries();
     let physline = new TimeSeries();
     let swapline = new TimeSeries();
     let battline = new TimeSeries();
+    let depyline = new TimeSeries();
+    let devtline = new TimeSeries();
     cpusgraf.addTimeSeries(cpusline, linestyl);
     physgraf.addTimeSeries(physline, linestyl);
     swapgraf.addTimeSeries(swapline, linestyl);
     battgraf.addTimeSeries(battline, linestyl);
+    dediphys.addTimeSeries(depyline, linestyl);
+    dedivirt.addTimeSeries(devtline, linestyl);
     let prcpgraf = [];
     let prcpline = [];
     let cyclgraf = [];
@@ -85,7 +108,7 @@ async function OverviewGraphAJAX() {
                     maxValue: deadobjc.cpuclock[indx]["max"],
                     grid: {
                         strokeStyle: '#c0c0c0',
-                        fillStyle: '#ffffff',
+                        fillStyle: 'rgba(0, 0, 0, 0)',
                         lineWidth: 1,
                         millisPerLine: 250,
                         verticalSections: 10,
@@ -140,10 +163,16 @@ async function OverviewGraphAJAX() {
                         cpusline.append(new Date().getTime(), parseFloat(liveobjc.cpuprcnt[indx]).toPrecision(3));
                         document.getElementById("cpusvalu").innerText = parseFloat(liveobjc.cpuprcnt[indx]).toPrecision(3);
                     }
+                    depyline.append(new Date().getTime(), parseFloat(liveobjc.virtdata["percent"]).toPrecision(3));
+                    document.getElementById("phys-dedi-byte").innerText = parseInt(liveobjc.virtdata["used"]) + " of " + parseInt(liveobjc.virtdata["total"]);
                     physline.append(new Date().getTime(), parseFloat(liveobjc.virtdata["percent"]).toPrecision(3));
                     document.getElementById("physvalu").innerText = parseFloat(liveobjc.virtdata["percent"]).toPrecision(3);
+                    document.getElementById("phys-dedi-perc").innerText = parseInt(liveobjc.virtdata["percent"]);
+                    devtline.append(new Date().getTime(), parseFloat(liveobjc.swapinfo["percent"]).toPrecision(3));
+                    document.getElementById("virt-dedi-byte").innerText = parseInt(liveobjc.swapinfo["used"]) + " of " + parseInt(liveobjc.swapinfo["total"]);
                     swapline.append(new Date().getTime(), parseFloat(liveobjc.swapinfo["percent"]).toPrecision(3));
                     document.getElementById("swapvalu").innerText = parseFloat(liveobjc.swapinfo["percent"]).toPrecision(3);
+                    document.getElementById("virt-dedi-perc").innerText = parseInt(liveobjc.swapinfo["percent"]);
                     battline.append(new Date().getTime(), parseFloat(liveobjc.sensread.battstat["percent"]).toPrecision(3));
                     document.getElementById("battvalu").innerText = parseFloat(liveobjc.sensread.battstat["percent"]).toPrecision(3);
                     for (let indx = 0; indx < cpuquant; indx ++) {
@@ -158,10 +187,29 @@ async function OverviewGraphAJAX() {
                     document.getElementById("cpuu-stat-intr").innerText = liveobjc.cpustats["interrupts"];
                     document.getElementById("cpuu-stat-soft").innerText = liveobjc.cpustats["soft_interrupts"];
                     document.getElementById("cpuu-stat-syst").innerText = liveobjc.cpustats["syscalls"];
+                    // Physical memory dedicated statistics table updater
+                    document.getElementById("memo-phys-totl").innerText = liveobjc.virtdata["total"];
+                    document.getElementById("memo-phys-free").innerText = liveobjc.virtdata["available"];
+                    document.getElementById("memo-phys-used").innerText = liveobjc.virtdata["used"];
+                    document.getElementById("memo-phys-actv").innerText = liveobjc.virtdata["active"];
+                    document.getElementById("memo-phys-inac").innerText = liveobjc.virtdata["inactive"];
+                    document.getElementById("memo-phys-buff").innerText = liveobjc.virtdata["buffers"];
+                    document.getElementById("memo-phys-cach").innerText = liveobjc.virtdata["cached"];
+                    document.getElementById("memo-phys-shar").innerText = liveobjc.virtdata["shared"];
+                    document.getElementById("memo-phys-slab").innerText = liveobjc.virtdata["slab"];
+                    // Virtual memory dedicated statistics table updater
+                    document.getElementById("memo-virt-totl").innerText = liveobjc.swapinfo["total"];
+                    document.getElementById("memo-virt-free").innerText = liveobjc.swapinfo["free"];
+                    document.getElementById("memo-virt-used").innerText = liveobjc.swapinfo["used"];
+                    document.getElementById("memo-virt-sine").innerText = liveobjc.swapinfo["sin"];
+                    document.getElementById("memo-virt-sout").innerText = liveobjc.swapinfo["sout"];
+                    // Stream graphs to DOM Canvas elements
                     cpusgraf.streamTo(document.getElementById("cpusover"), 1000);
                     physgraf.streamTo(document.getElementById("physover"), 1000);
                     swapgraf.streamTo(document.getElementById("swapover"), 1000);
                     battgraf.streamTo(document.getElementById("battover"), 1000);
+                    dediphys.streamTo(document.getElementById("phys-dedi-graf"), 1000);
+                    dedivirt.streamTo(document.getElementById("virt-dedi-graf"), 1000);
                     for (let indx = 0; indx < cpuquant; indx ++) {
                         cyclgraf[indx].streamTo(document.getElementById("cpuu-cygf-" + indx), 1000);
                         prcpgraf[indx].streamTo(document.getElementById("cpuu-graf-" + indx), 1000);
