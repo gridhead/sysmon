08ea02242ad0d9ac773606819d47c9bccbd47247
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2020-12-15 23:41:50 +0530

    Version bumped frontend service to v1.0.1
    Sorted imports and removed useless additions
    
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- frontend/main.py
+++ frontend/main.py
@@ -1,4 +1,4 @@
-'''
+"""
 ##########################################################################
 *
 *   Copyright © 2019-2020 Akashdeep Dhar <t0xic0der@fedoraproject.org>
@@ -17,13 +17,12 @@
 *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
 ##########################################################################
-'''
+"""
 
-from flask import Flask, render_template, jsonify
-
-import back
-import click
 import logging
+
+import click
+from flask import Flask, render_template
 
 
 main = Flask(__name__)
@@ -31,92 +30,27 @@ loge = logging.getLogger("werkzeug")
 loge.setLevel(logging.ERROR)
 
 
-@main.route("/termproc/<prociden>/", methods=["GET"])
-def termproc(prociden):
-    back.TerminateSingleProcess(prociden)
-    return "Terminated"
-
-
-@main.route("/killproc/<prociden>/", methods=["GET"])
-def killproc(prociden):
-    back.KillSingleProcess(prociden)
-    return "Killed"
-
-
-@main.route("/sspdproc/<prociden>/", methods=["GET"])
-def sspdroc(prociden):
-    back.SuspendSingleProcess(prociden)
-    return "Suspended"
-
-
-@main.route("/resmproc/<prociden>/", methods=["GET"])
-def resmproc(prociden):
-    back.ResumeSingleProcess(prociden)
-    return "Resumed"
-
-
-@main.route("/fetcinfo/", methods=["GET"])
-def fetcinfo():
-    virtdata = back.GetVirtualMemoryData()
-    swapinfo = back.GetSwapMemoryInfo()
-    cputimes = back.GetCPUStateTimes()
-    cpuprcnt = back.GetCPUUsagePercent()
-    cpustats = back.GetCPUStatistics()
-    cpuclock = back.GetCPUClockSpeed()
-    diousage = back.GetDiskIOUsage()
-    netusage = back.GetNetworkIOUsage()
-    procinfo = back.GetProcessInfo()
-    senstemp = back.GetSensorsTemperature()
-    fanspeed = back.GetSensorsFanSpeed()
-    battstat = back.GetSensorsBatteryStatus()
-    retnjson = jsonify(virtdata=virtdata, swapinfo=swapinfo, cputimes=cputimes,
-                       cpuprcnt=cpuprcnt, cpustats=cpustats, cpuclock=cpuclock,
-                       diousage=diousage, netusage=netusage, procinfo=procinfo,
-                       senstemp=senstemp, fanspeed=fanspeed, battstat=battstat)
-    return retnjson
-
-
 @main.route("/")
 def graphing():
     return render_template("mainpytm.html")
-
-
-@main.route("/<thmcolor>/", methods=["GET"])
-def custpage(thmcolor="maroon"):
-    retndata = back.GetOSUnameData()
-    cpuquant = back.GetCPULogicalCount()
-    diskpart = back.GetAllDiskPartitions()
-    diousage = back.GetDiskIOUsage()
-    netusage = back.GetNetworkIOUsage()
-    procinfo = back.GetProcessInfo()
-    senstemp = back.GetSensorsTemperature()
-    fanspeed = back.GetSensorsFanSpeed()
-    boottime = back.GetBootTime()
-    netaddrs = back.GetNetworkIFAddresses()
-    netstats = back.GetNetworkStatistics()
-    return render_template("custpage.html", retndata=retndata, cpuquant=cpuquant,
-                           diskpart=diskpart, diousage=diousage, netusage=netusage,
-                           netaddrs=netaddrs, netstats=netstats, senstemp=senstemp,
-                           fanspeed=fanspeed, boottime=boottime, procinfo=procinfo,
-                           thmcolor=thmcolor)
 
 
 @click.command()
 @click.option("-p", "--portdata", "portdata", help="Set the port value [0-65536]", default="9696")
 @click.option("-6", "--ipprotv6", "netprotc", flag_value="ipprotv6", help="Start the server on an IPv6 address")
 @click.option("-4", "--ipprotv4", "netprotc", flag_value="ipprotv4", help="Start the server on an IPv4 address")
-@click.version_option(version="0.1.0", prog_name="WebStation SYSMON by t0xic0der")
+@click.version_option(version="1.0.1", prog_name=click.style("SuperVisor Frontend Service", fg="magenta"))
 def mainfunc(portdata, netprotc):
-    print(" * Starting WebStation SYSMON by t0xic0der...")
-    print(" * Port number  : " + str(portdata))
+    click.echo(" * " + click.style("SuperVisor Frontend Service v1.0.1", fg="green"))
+    click.echo(" * " + click.style("Port number  ", fg="magenta") + ": " + str(portdata))
     netpdata = ""
     if netprotc == "ipprotv6":
-        print(" * IP version   : 6")
+        click.echo(" * " + click.style("IP version   ", fg="magenta") + ": " + "6")
         netpdata = "::"
     elif netprotc == "ipprotv4":
-        print(" * IP version   : 4")
+        click.echo(" * " + click.style("IP version   ", fg="magenta") + ": " + "4")
         netpdata = "0.0.0.0"
-    print(" * Logs state   : Errors only")
+    click.echo(" * " + click.style("Logs state   ", fg="magenta") + ": " + "Errors only")
     main.config["TEMPLATES_AUTO_RELOAD"] = True
     main.run(port=portdata, host=netpdata)
 
